fec234201880f8332026cb2dafc9bbf391a00763
Claudio Cruz <claudiocruz1989@gmail.com>
2022-10-19 03:55:22 +0000

    Deploy commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://qfejlggjnppjov:054d9f079e3ed6ec9075a8c558bff2a64ba10b664eb345e1d610c3cfa803990a@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d5ri9g8p0fo9tk"
+os.environ["SECRET_KEY"] = "randomSecretKey1881!!"
+os.environ["CLOUDINARY_URL"] = "cloudinary://416872162386444:PfAJG_eTx29nb8BAUnyTiHt3sAg@djmk0qtgj"
