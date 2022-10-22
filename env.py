4b6fdde5cfaf437dd6a18633f92bc181749053e0
Claudio Cruz <claudiocruz1989@gmail.com>
2022-10-22 08:08:55 +0000

    Add PostAdmin and CommentAdmin to admin.py

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ["DATABASE_URL"] = "postgres://qfejlggjnppjov:054d9f079e3ed6ec9075a8c558bff2a64ba10b664eb345e1d610c3cfa803990a@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d5ri9g8p0fo9tk"
+os.environ["DATABASE_URL"] = "postgres://pycdtwbtdvdios:55618f24f6ac7ddaaa869b620a43c247b84d51555ff7bfec659a733d3bd2870d@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/d69fi8e4rq0p74"
 os.environ["SECRET_KEY"] = "randomSecretKey1881!!"
 os.environ["CLOUDINARY_URL"] = "cloudinary://416872162386444:PfAJG_eTx29nb8BAUnyTiHt3sAg@djmk0qtgj"
